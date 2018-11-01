ac3e5a9d3d506e008d696a5aee5c97f7a90d892e
Krish <krishc8@hotmail.com>
2018-10-31 20:31:10 -0400

    icons done

--- Assignment2/src/ca/utoronto/utm/paint/shapeChooserButton.java
+++ Assignment2/src/ca/utoronto/utm/paint/shapeChooserButton.java
@@ -1,6 +1,10 @@
 package ca.utoronto.utm.paint;
 
+import java.net.URL;
+
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class shapeChooserButton extends Button {
 	String currentType;
@@ -14,7 +18,22 @@ public class shapeChooserButton extends Button {
 		return this.currentType;
 	}
 	public void handle() {
-		
+		Image currentImage = null;
+		if (this.currentType=="Circle") {
+			currentImage = new Image("file:circle.png",20,20,true,true);
+		}
+		else if (this.currentType=="Rectangle") {
+			currentImage = new Image("file:rectangle.png",20,20,true,true);
+		}
+		else if (this.currentType=="Square") {
+			currentImage = new Image("file:square.png",20,20,true,true);
+		}
+		else if (this.currentType=="Squiggle") {
+			currentImage = new Image("file:squiggle.png",20,20,true,true);
+		}
+		else if (this.currentType=="Polyline") {
+			currentImage = new Image("file:polygon.png",20,20,true,true);
+		}
+		this.setGraphic(new ImageView(currentImage));
 	}
-	
 }
